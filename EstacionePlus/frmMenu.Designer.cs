b6ffdf9516e2aba55d7fa6118861dd2dae4c5a22
Clayton Rocha <clayton.will08@gmail.com>
2024-10-27 16:10:00 -0300

    Identificando Foto de Placas

---
+++ EstacionePlus/frmMenu.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace EstacionePlus
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenu));
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnSelectImagem = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(88, 141);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(528, 116);
+            this.txtResult.TabIndex = 1;
+            this.txtResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnSelectImagem
+            // 
+            this.btnSelectImagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectImagem.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectImagem.Name = "btnSelectImagem";
+            this.btnSelectImagem.Size = new System.Drawing.Size(679, 29);
+            this.btnSelectImagem.TabIndex = 2;
+            this.btnSelectImagem.Text = "Selecionar Imagem";
+            this.btnSelectImagem.UseVisualStyleBackColor = true;
+            this.btnSelectImagem.Click += new System.EventHandler(this.btnSelectImagem_Click);
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(703, 353);
+            this.Controls.Add(this.btnSelectImagem);
+            this.Controls.Add(this.txtResult);
+            this.DoubleBuffered = true;
+            this.Name = "frmMenu";
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.frmMenu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnSelectImagem;
+    }
+}
+
---
+++ EstacionePlus/frmMenu.cs
@@ -0,0 +1,74 @@
+﻿using EmguCV = Emgu.CV; // Alias para Emgu CV
+using OpenCvSharp; // Usando OpenCvSharp
+using System;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace EstacionePlus
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMenu_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnSelectImagem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\"; // Diretório inicial
+                openFileDialog.Filter = "Imagens (*.jpg;*.jpeg;*.png)|*.jpg;*.jpeg;*.png"; // Filtros para tipos de arquivos
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string imagePath = openFileDialog.FileName; // Obtém o caminho da imagem selecionada
+                    ProcessarImagem(imagePath); // Chama o método para processar a imagem
+                }
+            }
+        }
+
+        private void ProcessarImagem(string imagePath)
+        {
+            string tessDataPath = @"C:\Users\Emil\source\repos\EstacionePlus\tessdata";
+
+            try
+            {
+                // Carregar a imagem com OpenCV
+                Mat image = Cv2.ImRead(imagePath, ImreadModes.Grayscale);
+
+                // Aumentar o contraste e o brilho
+                image.ConvertTo(image, MatType.CV_8U, alpha: 1.5, beta: 20); // Ajuste os valores conforme necessário
+
+                // Aplicar um filtro de desfoque leve para reduzir ruído
+                Cv2.GaussianBlur(image, image, new OpenCvSharp.Size(3, 3), 0); // Usando OpenCvSharp.Size
+
+                // Salvar a imagem processada para revisão (opcional)
+                Cv2.ImWrite(@"C:\Users\Emil\source\repos\EstacionePlus\placas\exemplo_preprocessado.jpg", image);
+
+                // Processar a imagem com o Tesseract
+                var ocrEngine = new TesseractEngine(tessDataPath, "por", EngineMode.Default);
+                ocrEngine.SetVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+
+                var page = ocrEngine.Process(Pix.LoadFromFile(@"C:\Users\Emil\source\repos\EstacionePlus\placas\exemplo_preprocessado.jpg"));
+                string textoExtraido = page.GetText();
+
+                // Exibir o texto extraído no TextBox
+                txtResult.Text = textoExtraido; // Adiciona o texto extraído ao TextBox
+
+                Console.WriteLine("Texto Extraído da Imagem:");
+                Console.WriteLine(textoExtraido);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro durante o OCR: " + ex.Message);
+            }
+        }
+    }
+}
